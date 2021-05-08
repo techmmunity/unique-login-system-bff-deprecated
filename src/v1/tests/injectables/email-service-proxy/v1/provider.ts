ba31c27b368faeda943660c31456944cc6f3fc77
Henrique Leite <henriqueleite616@gmail.com>
2021-05-07 21:42:28 -0300

    Fix EmailServiceProxy Folder Name

--- src/v1/tests/injectables/email-service-proxy/v1/provider.ts
+++ src/v1/tests/injectables/email-service-proxy/v1/provider.ts
@@ -1,5 +1,5 @@
 import { Provider } from "@nestjs/common";
-import { EMAIL_SERVICE_PROXY_V1_PROVIDER_ID } from "v1/injectables/email-service-proxy copy/v1/email-service-proxy-v1.provider";
+import { EMAIL_SERVICE_PROXY_V1_PROVIDER_ID } from "v1/injectables/email-service-proxy/v1/email-service-proxy-v1.provider";
 
 import { EmailServiceProxyV1MockType } from "./types";
 
